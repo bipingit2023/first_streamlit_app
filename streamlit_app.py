b19d87598f56d3b254aa7d623499d4ebe841d4b3
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-12 21:42:27 +0530

    Update streamlit_app.py
    parsing json on screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # This just writes the json on screen
+
+# take json version of the response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# printing the normalized json as DF on screen
+streamlit.dataframe(fruityvice_normalized)
