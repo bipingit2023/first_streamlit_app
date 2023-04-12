e8f0a6f21ff4d70c76f2f1ef90eb605dabfde155
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-13 00:57:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ def get_fruityvice_data(this_fruit_choice):
          # streamlit.dataframe(fruityvice_normalized)
          return fruityvice_normalized
       
-back_from_function = get_fruityvice_data(back_from_function)
+back_from_function = get_fruityvice_data
+back_from_function
 
 #except URLError as e:
   #streamlit.error() 
