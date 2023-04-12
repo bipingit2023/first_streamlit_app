4344892d832570ef533a9542051912c4c74a2a40
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-13 01:28:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,22 +56,24 @@ except URLError as e:
   streamlit.error() 
   
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT * FROM fruit_load_list")
 # my_data_row = my_cur.fetchone()
 # streamlit.text("The fruit load list contains:")
 # streamlit.text(my_data_row)
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# my_data_rows = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_rows)
 
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #streamlit.write('Thanks for adding ', add_my_fruit)
 
 #my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+
+streamlit.header("The fruit load list contains:")
 
 def insert_row_snowflake(new_fruit):
  with my_cnx.cursor() as my_cur:
