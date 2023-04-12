e039eb34f6cc12b56411c91a745980dbd53241d3
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-13 00:53:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ def get_fruityvice_data(this_fruit_choice):
          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
          # printing the normalized json as DF on screen
          # streamlit.dataframe(fruityvice_normalized)
-return fruityvice_normalized
+         return fruityvice_normalized
       
 back_from_function = get_fruityvice_data(fruit_choice)
 back_from_function
