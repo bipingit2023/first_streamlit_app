a58e416f4212ed113273c1e2602ca706a2a83c39
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-13 01:13:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,22 +38,22 @@ streamlit.dataframe(fruits_to_show)
  # if not fruit_choice:
  #    streamlit.error("Please select a fruit to get information.")
  # else:
-     # create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):         
-         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-         # streamlit.text(fruityvice_response.json()) # This just writes the json on screen
-         # take json version of the response and normalize it 
-         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-         # printing the normalized json as DF on screen
-         # streamlit.dataframe(fruityvice_normalized)
-         return fruityvice_normalized
-      
-back_from_function = get_fruityvice_data
-streamlit.dataframe(back_from_function)
-
-#except URLError as e:
-  #streamlit.error() 
+# create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):        
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     return fruityvice_normalized
+   
+streamlit.header('Fruityvice Fruit Advice!') 
+try:   
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')   
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else: 
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error() 
   
 # don't run anything past here while we troubleshoot
 streamlit.stop()
