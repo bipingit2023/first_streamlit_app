76b54e2b10f8a49abf5899ab66c8a8c4c9acb6da
bipingit2023 <130430314+bipingit2023@users.noreply.github.com>
2023-04-13 01:05:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,9 @@ streamlit.dataframe(fruits_to_show)
  #    streamlit.error("Please select a fruit to get information.")
  # else:
      # create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+def get_fruityvice_data(this_fruit_choice):         
+         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
          # streamlit.text(fruityvice_response.json()) # This just writes the json on screen
          # take json version of the response and normalize it 
          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
